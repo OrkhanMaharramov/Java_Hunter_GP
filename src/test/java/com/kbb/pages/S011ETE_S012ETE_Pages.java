9953aa64c28f0dacb05a5deea0bab8dfcd3ea9b5
ozgenoktem <oktem.ozgen@gmail.com>
2018-03-20 17:55:21 -0400

    Ozgen added testCase

---
+++ src/test/java/com/kbb/pages/S011ETE_S012ETE_Pages.java
@@ -0,0 +1,92 @@
+package com.kbb.pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class S011ETE_S012ETE_Pages {
+	
+private WebDriver driver;
+	
+	public S011ETE_S012ETE_Pages(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="(//ul[@class='contentlist make-list no-bull left']//a[@data-omn-partner='ford'])[1]")
+	public WebElement fordButton;
+	
+	@FindBy(linkText="No, thanks")
+	public WebElement noThanksPopUp;
+	
+	@FindBy(xpath="//h1[@class='title white no-margin with-badge']")
+	public WebElement fordTrucksHeader;
+	
+	@FindBy(xpath="//span[contains(text(),'Ford F150')]")
+	public WebElement F150button;
+	
+	@FindBy(xpath="//div[@class='right']//a[@href='/ford/f150-regular-cab/?vehicleclass=newcar&intent=buy-new&category=pickup']")
+	public WebElement F150regularSelectButton;
+	
+	@FindBy(xpath="//h2[contains(text(),'Select a Year')]")
+	public WebElement selectYearHeader;
+	
+	@FindBy(xpath="//a[contains(text(),'Select 2017')]")
+	public WebElement select2017button;
+	
+	@FindBy(xpath="//a[@data-selected-vehicle-styles='XL']")
+	public WebElement selectXLbutton;
+	
+	@FindBy(xpath="//input[@value='421652']")
+	public WebElement fourWheelDrive;
+	
+	@FindBy(xpath="//span[contains(text(),'Colors and Interiors')]")
+	public WebElement colorsInteriors;
+	
+	@FindBy(id="rdOptionId7524903")
+	public WebElement metalicColor;
+	
+	@FindBy(id="rdErrorFor7524712")
+	public WebElement darkEarthGreyColor;
+	
+	@FindBy(linkText="Submit")
+	public WebElement submitButton;
+	
+	@FindBy(id="GetMyPrice")
+	public WebElement seeFairPrice;
+	
+	@FindBy(xpath="//h1[@class='color-a']//span[contains(text(),'2017 Ford F150 Regular Cab')]")
+	public WebElement choosenCar;
+	
+	@FindBy(xpath="//span[contains(text(),'Pricing')]")
+	public WebElement pricing;
+	
+	@FindBy(id="configuredPrice")
+	public WebElement price;
+	
+	@FindBy(id="RangeBox")
+	public WebElement ranges;
+	
+	@FindBy(id="invoiceBox")
+	public WebElement invoives;
+	
+	@FindBy(id="msrpBox")
+	public WebElement msrp;
+	
+	public void chooseButton(WebElement element) {
+		//WebElement element = select2017button;
+		JavascriptExecutor jse = (JavascriptExecutor)driver; 
+		jse.executeScript("var evt = document.createEvent('MouseEvents');" + "evt.initMouseEvent('click',true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0,null);" + "arguments[0].dispatchEvent(evt);", element);
+	}
+	
+
+	public void clickButton(WebElement element) {
+		element.click();
+	}
+	
+	
+	
+
+}
--- src/test/java/com/kbb/tests/search/S011ETE_S012ETE.java
+++ src/test/java/com/kbb/tests/search/S011ETE_S012ETE.java
@@ -6,7 +6,8 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 
 import com.kbb.pages.HomePage;
-import com.kbb.pages.OzgenTestsPages11_12;
+import com.kbb.pages.S011ETE_S012ETE_Pages;
+import com.kbb.pages.VolvoS60PricingPage;
 import com.kbb.pages.PriceYourNextCarPage;
 import com.kbb.pages.VolvoS60SelectPage;
 import com.kbb.utilities.TestBaseClass;
@@ -36,7 +37,7 @@ public class S011ETE_S012ETE extends TestBaseClass{
 		assertTrue(driver.getTitle().contains("Trucks")); //step4 assertion
 		
 	//	(//ul[@class='contentlist make-list no-bull left']//a[@data-omn-partner='ford'])[1]
-		OzgenTestsPages11_12 test11_12=new OzgenTestsPages11_12(driver);
+		S011ETE_S012ETE_Pages test11_12=new S011ETE_S012ETE_Pages(driver);
 		
 		test11_12.clickButton(test11_12.fordButton); //STEP 5
 		assertTrue(driver.getTitle().contains("Ford Pickup Models")); //step 5 assertion
@@ -51,9 +52,31 @@ public class S011ETE_S012ETE extends TestBaseClass{
 		assertTrue(driver.getTitle().contains("Ford F150 Regular Cab")); //step 7 assertion
 		assertTrue(test11_12.selectYearHeader.isDisplayed());	//step 7 assertion
 		
-		test11_12.clickButton(test11_12.select2017button); //STEP 8
+		test11_12.chooseButton(test11_12.select2017button); //Step 8
+		assertTrue(driver.getTitle().contains("2017 Ford F150 Regular Cab Styles")); //Step 8 Assertion
 		
+		test11_12.chooseButton(test11_12.selectXLbutton); //Step 9
+		assertTrue(driver.getTitle().contains("2017 Ford F150 Regular Cab XL Options")); //Step 9 Assertion
 		
+		test11_12.chooseButton(test11_12.fourWheelDrive);//Step 10
+		
+		test11_12.chooseButton(test11_12.colorsInteriors); //Step 11
+		test11_12.chooseButton(test11_12.metalicColor); //Step 12
+		test11_12.chooseButton(test11_12.darkEarthGreyColor); //Step 13
+		test11_12.chooseButton(test11_12.submitButton); //Step 14
+		test11_12.chooseButton(test11_12.seeFairPrice); //Step 15
+		
+		System.out.println(test11_12.choosenCar.getText()+" "+test11_12.pricing.getText());
+		assertTrue(test11_12.choosenCar.isDisplayed()); //Step 16
+		assertTrue(test11_12.pricing.isDisplayed()); //Step 16
+		
+		VolvoS60PricingPage pricing=new VolvoS60PricingPage(driver); 
+		System.out.println(pricing.verifyPrice(test11_12.price)); //Step 17
+		assertTrue(0<pricing.verifyPrice(test11_12.price));
+		
+	//	assertTrue(test11_12.ranges.isDisplayed());    
+	//	assertTrue(test11_12.invoives.isDisplayed());
+	//	assertTrue(test11_12.msrp.isDisplayed());
 		
 	}
 	  
